9a2ca133b621529c976c3f5fcf1062c5228048e0
Dexter Quiambao <dexterquiambao@googlemail.com>
2020-11-12 12:54:30 +0000

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # added root to - not sure if this is required?
+  root to: "cocktails#index"
+  # or root 'cocktails#index' is a shortcut for the above code - Source: Ruby Docs > routing
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
 end
